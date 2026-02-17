6bc9f3702a77f58adc956e152e54ec8d0d2ea7c9
Abdelhadi-91 <abdhedrez02@gmail.com>
2026-02-18 00:32:53 +0100

    Add input prompt for string in ex1 and create ex3 for text transformation

--- TP3/ex1.java
+++ TP3/ex1.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class ex1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
         String str = scanner.nextLine();
         if (str.isEmpty()) {
             System.out.println("The string is empty");
