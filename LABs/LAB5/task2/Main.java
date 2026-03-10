3f2916ad9b3c2fcbd560a57aa5faaf4fe5e04bd1
Abdelhadi-91 <abdhedrez02@gmail.com>
2026-03-11 00:46:20 +0100

    Update FsItem instantiation with new parameters and adjust output formatting

--- LABs/LAB5/task2/Main.java
+++ LABs/LAB5/task2/Main.java
@@ -1,19 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        FsItem note = new TextFile("myNote", 2, 1500);
-        FsItem pic = new ImageFile("myPicture", 10, 1080);
+        FsItem note = new TextFile("myNote", 2.2, 1500);
+        FsItem pic = new ImageFile("myPicture", 22, 2160);
 
         System.out.println(note.info());
+        System.out.println("---");
         System.out.println(pic.info());
-
+        System.out.println("---");
         note.rename("projectDescription");
-
+        System.out.println("---");
         pic.rename(null);
+        System.out.println("---");
         pic.rename("");
+        System.out.println("---");
         pic.rename("                ");
+        System.out.println("---");
         pic.rename("pic/of/myHome");
+        System.out.println("---");
         pic.rename("myPicture");
+        System.out.println("---");
         pic.rename("picture1");
+        System.out.println("---");
 
         System.out.println(pic.info());
 
